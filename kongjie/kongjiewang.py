cd90096b333aeed246aacbfbaf4b0ec6ba8ef105
ychen <ychenracing@163.com>
2020-03-22 22:05:50 +0800

    fix recursion depth issue

--- kongjie/kongjiewang.py
+++ kongjie/kongjiewang.py
@@ -4,6 +4,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import redis
+import queue
+
+album_url_queue = queue.Queue()
+img_url_queue = queue.Queue()
+
 
 save_folder = r'E:\Downloads\kongjiewang'
 domain_name = 'http://www.kongjie.com/'
@@ -40,57 +45,69 @@ def save_img(image_url, uid, picid):
         print('save error！error=%s, image_url=%s' % (e, image_url))
 
 
-def save_images_in_album(album_url):
+def save_images_in_album():
     """
     进入空姐网用户的相册，开始一张一张的保存相册中的图片。
     """
     # 解析出uid和picid，用于存储图片的名字
-    uid_picid_match = uid_picid_pattern.search(album_url)
-    if not uid_picid_match:
-        return
-    else:
-        uid = uid_picid_match.group(1)
-        picid = uid_picid_match.group(2)
+    while not img_url_queue.empty():
+        album_url = img_url_queue.get()
 
-    response = requests.get(album_url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
-    image_div = soup.find('div', id='photo_pic', class_='c')
-    if image_div and not redis_con.hexists('kongjiewang', uid + ':' + picid):
-        image_src = domain_name + image_div.a.img['src']
-        save_img(image_src, uid, picid)
-        redis_con.hset('kongjie', uid + ':' + picid, '1')
+        uid_picid_match = uid_picid_pattern.search(album_url)
+        if not uid_picid_match:
+            return
+        else:
+            uid = uid_picid_match.group(1)
+            picid = uid_picid_match.group(2)
 
-    next_image = soup.select_one('div.pns.mlnv.vm.mtm.cl a.btn[title="下一张"]')
-    if not next_image:
-         return
-    # 解析下一张图片的picid，防止重复爬取图片，不重复则抓取
-    next_image_url = next_image['href']
-    next_uid_picid_match = uid_picid_pattern.search(next_image_url)
-    if not next_uid_picid_match:
-        return
-    next_uid = next_uid_picid_match.group(1)
-    next_picid = next_uid_picid_match.group(2)
-    if not redis_con.hexists('kongjie', next_uid + ':' + next_picid):
-        save_images_in_album(next_image_url)
+        response = requests.get(album_url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        image_div = soup.find('div', id='photo_pic', class_='c')
+        if image_div and not redis_con.hexists('kongjiewang', uid + ':' + picid):
+            image_src = image_div.a.img['src']
+            save_img(image_src, uid, picid)
+            redis_con.hset('kongjie', uid + ':' + picid, '1')
+
+        next_image = soup.select_one('div.pns.mlnv.vm.mtm.cl a.btn[title="下一张"]')
+        if not next_image:
+             return
+        # 解析下一张图片的picid，防止重复爬取图片，不重复则抓取
+        next_image_url = next_image['href']
+        next_uid_picid_match = uid_picid_pattern.search(next_image_url)
+        if not next_uid_picid_match:
+            return
+        next_uid = next_uid_picid_match.group(1)
+        next_picid = next_uid_picid_match.group(2)
+        if not redis_con.hexists('kongjie', next_uid + ':' + next_picid):
+            img_url_queue.put(next_image_url)
 
 
-def parse_album_url(url):
+def parse_album_url():
     """
     解析出相册url，然后进入相册爬取图片
     """
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')
-    people_list = soup.select('div.ptw li.d')
-    for people in people_list:
-        save_images_in_album(people.div.a['href'])
+    while not album_url_queue.empty():
+        url = album_url_queue.get()
 
-    # 爬取下一页
-    next_page = soup.select_one('a.nxt')
-    if next_page:
-        parse_album_url(next_page['href'])
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        people_list = soup.select('div.ptw li.d')
+        for people in people_list:
+            img_url_queue.put(people.div.a['href'])
+            save_images_in_album()
+
+        # 爬取下一页
+        next_page = soup.select_one('a.nxt')
+        if next_page:
+            album_url_queue.put(next_page['href'])
 
 if __name__ == '__main__':
-    parse_album_url(start_url)
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    album_url_queue.put(start_url)
+
+    parse_album_url()
 
 
 # Requests文档：http://docs.python-requests.org/zh_CN/latest/user/quickstart.html#id2
